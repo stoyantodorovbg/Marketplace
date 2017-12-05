9ab29f62878fe6573a889e5a51033d485fc69a15
stoyantodorovbg <stoyan_11@abv.bg>
2017-12-05 12:50:38 +0200

    Add CRUD for Category and Product

---
+++ src/AppBundle/Form/ProductType.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace AppBundle\Form;
+
+use AppBundle\Entity\Category;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ProductType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name')
+            ->add('description')
+            ->add('price')->add('currency')
+            ->add('availability')->add('quantity')
+            ->add('brand')->add('model')
+            ->add('isInPromotion')
+            ->add('categories', EntityType::class, [
+        'class' => 'AppBundle:Category',
+        'choice_label' => 'name'
+    ]       );
+    }
+    
+    /**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'AppBundle\Entity\Product'
+        ));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBlockPrefix()
+    {
+        return 'appbundle_product';
+    }
+
+
+}
